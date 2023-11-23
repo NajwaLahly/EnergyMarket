84baec65b073650d7033032f7965ada62d676563
najwalahly <najwa.lahly@gmail.com>
2023-11-23 13:11:52 +0100

    init project

---
+++ src/main/java/com/example/energymarket/EnergymarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.energymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergymarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergymarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/energymarket/EnergymarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.energymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergymarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
